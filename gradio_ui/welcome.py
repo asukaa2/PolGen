91a5a50def6a050fc492ce90ad466786dfa3399e
asukaa2 <zavoxpy@gmail.com>
2026-09-20 11:25:03 +0700

    Update welcome.py

--- gradio_ui/welcome.py
+++ gradio_ui/welcome.py
@@ -2,14 +2,14 @@ import gradio as gr
 
 
 def welcome_tab():
-    gr.HTML("<center><h1 style='font-size: 3em;'><b>Добро пожаловать в PolGen</b></h1></center>")
+    gr.HTML("<center><h1 style='font-size: 3em;'><b>Welcome to PolGen</b></h1></center>")
     with gr.Row():
         with gr.Column(variant="panel"):
-            gr.HTML("<center><h2><a href='https://t.me/Politrees2'>Telegram ЛС</a></h2></center>")
-            gr.HTML("<center><h2><a href='https://vk.com/artem__bebroy'>ВКонтакте</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://t.me/Politrees2'>Telegram DM</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://vk.com/artem__bebroy'>VKontakte</a></h2></center>")
         with gr.Column(variant="panel"):
-            gr.HTML("<center><h2><a href='https://t.me/pol1trees'>Telegram Канал</a></h2></center>")
-            gr.HTML("<center><h2><a href='https://t.me/+GMTP7hZqY0E4OGRi'>Telegram Чат</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://t.me/pol1trees'>Telegram Channel</a></h2></center>")
+            gr.HTML("<center><h2><a href='https://t.me/+GMTP7hZqY0E4OGRi'>Telegram Chat</a></h2></center>")
 
     with gr.Column(variant="panel"):
         gr.HTML("<center><h2><a href='https://www.youtube.com/@Politrees'>YouTube</a></h2></center>")
